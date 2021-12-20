8dd3b06df947e209863e191549a4210f5d9b85cd
Tyler Long <tyler4long@gmail.com>
2021-12-20 15:30:36 -0800

    First real GitHub API call

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import axios from 'axios';
+// import {Octokit} from '@octokit/core';
+import {
+  // restEndpointMethods,
+  RestEndpointMethodTypes,
+} from '@octokit/plugin-rest-endpoint-methods';
 
 const client = axios.create({
   baseURL: 'https://api.github.com/',
 });
 
+// const MyOctokit = Octokit.plugin(restEndpointMethods);
+// const octokit = new MyOctokit();
+
 const main = async () => {
-  const r = await client.get('/zen');
-  console.log(r.data);
+  const r = (await client.get(
+    '/repos/tylerlong/github-database/releases'
+  )) as RestEndpointMethodTypes['repos']['listReleases']['response'];
+  const data = r.data;
+  const release = data.filter(item => item.name === 'default')[0];
+  console.log(JSON.stringify(release, null, 2));
+
+  // const r = await octokit.rest.repos.listReleases({
+  //   owner: 'tylerlong',
+  //   repo: 'github-database',
+  // });
 };
 
 main();
