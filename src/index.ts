1ff92afec4e3d136d595fc6896a7a43f4be3d178
Tyler Long <tyler4long@gmail.com>
2021-12-20 11:48:30 -0800

    Try some GitHub API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com/',
+});
+
+const main = async () => {
+  const r = await client.get('/zen');
+  console.log(r.data);
+};
+
+main();
