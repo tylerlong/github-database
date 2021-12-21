143d862425b5c752916fe5dd1e4dba873be460c7
Tyler Long <tyler4long@gmail.com>
2021-12-20 21:14:38 -0800

    Gist S3

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,38 @@
 import {Octokit} from '@octokit/core';
 import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods';
 import {Api} from '@octokit/plugin-rest-endpoint-methods/dist-types/types';
-import hyperid from 'hyperid';
-
-const uuid = hyperid();
 
 const MyOctokit = Octokit.plugin(restEndpointMethods);
 
-type GitHubDatabaseOptions = {
-  token: string;
-  owner: string;
-  repo: string;
-  releaseTag: string;
+type GistS3Options = {
+  personalAccessToken: string;
+  gistId: string;
 };
 
-class GitHubDatabase {
-  options: GitHubDatabaseOptions;
+class GistS3 {
   octokit: Octokit & Api;
-  releaseId: number | null = null;
+  gistId: string;
 
-  constructor(options: GitHubDatabaseOptions) {
-    this.options = options;
-    this.octokit = new MyOctokit({auth: process.env.GH_TOKEN});
+  constructor(options: GistS3Options) {
+    this.octokit = new MyOctokit({auth: options.personalAccessToken});
+    this.gistId = options.gistId;
   }
 
-  async init() {
-    try {
-      const r = await this.octokit.rest.repos.getReleaseByTag({
-        owner: this.options.owner,
-        repo: this.options.repo,
-        tag: this.options.releaseTag,
-      });
-      this.releaseId = r.data.id;
-    } catch (e) {
-      const r = await this.octokit.rest.repos.createRelease({
-        owner: this.options.owner,
-        repo: this.options.repo,
-        tag_name: this.options.releaseTag,
-      });
-      this.releaseId = r.data.id;
-    }
-  }
-
-  async put(value: string): Promise<number> {
-    const r = await this.octokit.rest.repos.uploadReleaseAsset({
-      owner: this.options.owner,
-      repo: this.options.repo,
-      release_id: this.releaseId!,
-      name: `${uuid()}.txt`,
-      data: value,
+  async put(filename: string, content: string) {
+    await this.octokit.rest.gists.update({
+      gist_id: this.gistId,
+      files: {
+        [filename]: {content},
+      },
     });
-    return r.data.id;
   }
 
-  async get(id: number) {
-    await this.octokit.rest.repos.getReleaseAsset({
-      owner: this.options.owner,
-      repo: this.options.repo,
-      asset_id: id,
+  async get(filename: string): Promise<string | undefined> {
+    const r = await this.octokit.rest.gists.get({
+      gist_id: this.gistId,
     });
+    return r.data.files?.[filename]?.content;
   }
 }
 
-export default GitHubDatabase;
+export default GistS3;
