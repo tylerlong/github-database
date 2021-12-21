c4c2df6f5df3216782378b7bd71341866a1ee80e
Tyler Long <tyler4long@gmail.com>
2021-12-20 16:31:45 -0800

    Delete old assets

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,35 @@ const octokit = new MyOctokit({auth: process.env.GH_TOKEN});
 
 const owner = 'tylerlong';
 const repo = 'github-database';
+const release_id = 55697422;
 
 const main = async () => {
-  const r = await octokit.rest.repos.listReleases({owner, repo});
-  const release = r.data.filter(item => item.name === 'default')[0];
-  console.log(JSON.stringify(release, null, 2));
+  // get release
+  const r = await octokit.rest.repos.getRelease({
+    owner,
+    repo,
+    release_id,
+  });
 
+  // delete old assets
+  const oldAssets = r.data.assets.filter(
+    asset =>
+      Date.now() - new Date(asset.updated_at).getTime() > 24 * 60 * 60 * 1000
+  );
+  console.log(JSON.stringify(oldAssets, null, 2));
+  for (const oldAsset of oldAssets) {
+    await octokit.rest.repos.deleteReleaseAsset({
+      owner,
+      repo,
+      asset_id: oldAsset.id,
+    });
+  }
+
+  // upload new asset
   const r2 = await octokit.rest.repos.uploadReleaseAsset({
     owner,
     repo,
-    release_id: release.id,
+    release_id,
     name: 'hello.txt',
     data: 'world',
   });
